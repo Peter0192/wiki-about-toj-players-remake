1516ca1f7d08193d1c2a506dff6eabc247ae8ca2
Peter0192 <128909318+Peter0192@users.noreply.github.com>
2024-09-29 16:52:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function jellydevredirect() {
+  window.location.href = "\jellydev.html";
+}
+
+function peterredirect() {
+  window.location.href = "\peter.html";
+}
+
+function atoredirect() {
+  window.location.href = "\ato.html";
+}
+
+function brightredirect() {
+  window.location.href = "\bright.html";
+}
+
+function towerofjellyredirect() {
+  window.location.href = "http://www.roblox.com/games/8260051519/Tower-of-Jelly";
+}
+
+function goback() {
+  window.location.href = "\index.html";
+}
+
+function difficultyflingx() {
+  window.location.href = "http://www.roblox.com/pl/games/12480064676/Difficulty-Fling-X";
+}
